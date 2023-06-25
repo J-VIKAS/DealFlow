a24a9822fdb4d1671a354a070923524a8ea83636
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2023-06-25 15:51:55 +0530

    Update settings.py

--- DealFlow/settings.py
+++ DealFlow/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x=jh^6u*jo&uw2%__0agrz11#7c&n0^a__=@rh(n9@)%aklc!9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
